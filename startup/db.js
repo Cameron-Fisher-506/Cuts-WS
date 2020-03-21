6c1cf7b0e07e293623189b28f26d12031039abe2
Cameron <cameron@aheadofthecurve.co.za>
2020-03-21 16:19:55 +0200

    Push production Added MongoDB cluster

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 function connectionToMongoDB()
 {
-    let url = "mongodb://localhost:27017/cuts";
+    //let url = "mongodb://localhost:27017/cuts";
+    let url = "mongodb+srv://cameron:Cameronbomberfisher_506@cluster0-nnf6g.mongodb.net/test?retryWrites=true&w=majority"
     mongoose.connect(url, {useCreateIndex:true, useNewUrlParser: true, useFindAndModify: false})
         .then(() => console.log("Connected to mongodb..."))
         .catch((err) => console.log(err.message));
